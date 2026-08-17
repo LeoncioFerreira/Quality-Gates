dabf8eee367fdb92b1233dc8a9c8c1e4054e0362
Ramona <ramona.vitoria@aluno.ufca.edu.br>
2026-08-17 23:46:09 +0000

    fix: corrige erros de formatação

--- src/main/kotlin/sistema/alunos/model/Avaliacao.kt
+++ src/main/kotlin/sistema/alunos/model/Avaliacao.kt
@@ -4,23 +4,11 @@
  */
 package sistema.alunos.model
 
-
-
-
-
-data  class Avaliacao(
+data class Avaliacao(
     val aluno: Aluno,
-
-
-    
     val disciplina: Disciplina,
     val notas: List<Nota>,
 ) {
-
-
-
-
-
     init  {
     require(notas.size == 2) {
         "A avaliacao deve possuir exatamente duas notas."

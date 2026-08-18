4348a4fe69b4fbb0ec8a804f601bd5532950aec1
Ramona <ramona.vitoria@aluno.ufca.edu.br>
2026-08-18 19:33:50 +0000

    test: provoca falha do detekt

---
+++ src/main/kotlin/sistema/alunos/model/DetektExperimento.kt
@@ -0,0 +1,9 @@
+/**
+ * Descrição: Arquivo criado apenas para demonstrar uma falha do Detekt.
+ * Autor: Ramona Vitória
+ */
+package sistema.alunos.model
+
+class DetektExperimento {
+    fun exemplo(): Int = 999
+} 

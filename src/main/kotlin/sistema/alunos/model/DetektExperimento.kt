d56e706c9dd25c346be1affa8c5d14807697c3d0
Ramona <ramona.vitoria@aluno.ufca.edu.br>
2026-08-18 19:43:46 +0000

    fix: remove violação do Detekt

--- src/main/kotlin/sistema/alunos/model/DetektExperimento.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Descrição: Arquivo criado apenas para demonstrar uma falha do Detekt.
- * Autor: Ramona Vitória
- */
-package sistema.alunos.model
-
-class DetektExperimento {
-    fun exemplo(): Int = 999
-}

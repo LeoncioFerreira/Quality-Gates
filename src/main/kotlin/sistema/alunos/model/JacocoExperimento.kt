bb396bdd1a2ab2f7acb72652fea850266cc9113f
Ramona <ramona.vitoria@aluno.ufca.edu.br>
2026-08-18 19:56:22 +0000

    test: provoca falha do JaCoCo

---
+++ src/main/kotlin/sistema/alunos/model/JacocoExperimento.kt
@@ -0,0 +1,15 @@
+/**
+ * Descrição: Classe criada apenas para demonstrar reprovação do JaCoCo.
+ * Autor: Ramona Vitória.
+ */
+package sistema.alunos.model
+
+class JacocoExperimento {
+
+    fun soma(a: Int, b: Int): Int = a + b
+
+    fun multiplica(a: Int, b: Int): Int = a * b
+
+    fun ehPar(numero: Int): Boolean = numero % 2 == 0
+}
+git 
